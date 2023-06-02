c2f97e0709c9cbb9fcd1f1ce052364d22586c24b
DanielMayo <daniielmayo.mayo@gmail.com>
2023-06-02 14:25:17 -0500

    videos of the modified data

---
+++ controllers/videos.js
@@ -0,0 +1,99 @@
+const videos = [
+    {
+        id: 1,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "El nombre es el propósito/Hasta Jacob.",
+        url: "https://www.youtube.com/embed/MrZEACaZ1Z8",
+    },
+    {
+        id: 2,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: " Hasta los jueces / Olvida tu pasado, mira tu futuro.",
+        url: "https://www.youtube.com/embed/Fv8aSrgujyM",
+    },
+    {
+        id: 3,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "Instrucciones para un Rey | El corazón de David revelado",
+        url: "https://www.youtube.com/embed/E6MxtGB7ADE",
+    },
+    {
+        id: 4,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "Desde el diamante hasta los profetas. Llamando pecado a lo que Dios le llama.",
+        url: "https://www.youtube.com/embed/DOYmDb2lxEM",
+    },
+    {
+        id: 5,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "Hasta los exilios de Israel y Judá",
+        url: "https://www.youtube.com/embed/ZZ6107Dd7mA",
+    },
+    {
+        id: 6,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: " Sesión 6. Hasta Ciro Rey de Persia",
+        url: "https://www.youtube.com/embed/wlKfu9zXHLA",
+    },
+    {
+        id: 7,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: " Línea de tiempo e historia del hijo pródigo.",
+        url: "https://www.youtube.com/embed/_me6qohlqGI",
+    },
+    {
+        id: 8,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "¿Quién soy?",
+        url: "https://www.youtube.com/embed/r3-1xUMxgeM",
+    },
+    {
+        id: 9,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "Video 3",
+        url: "https://www.youtube.com/embed/b2SKe8OU6e4",
+    },
+    {
+        id: 10,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "La Ley de Moisés",
+        url: "https://www.youtube.com/embed/2KPLvPQpYOw",
+    },
+    {
+        id: 11,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "La Ley de Moisés",
+        url: "https://www.youtube.com/embed/dnETnQfMqXA",
+    },
+    {
+        id: 12,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "La Ley de Moisés",
+        url: "https://www.youtube.com/embed/KGDm_MZlr74",
+    },
+    {
+        id: 13,
+        curso: "curso basico biblico ",
+        tema: 1,
+        title: "La Ley de Moisés",
+        url: "https://www.youtube.com/embed/c4KYMevjbAw",
+    },
+
+    // ... otros videos
+];
+
+module.exports = {
+    videos,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,50 @@
 // Rutas de usuarios /Auth
 // host + /api/auth
 
-const { Router } = require('express')
-const { check } = require('express-validator')
-const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
-const { validarCampos } = require('../middlewares/validar-campos')
-const { validarJWT }= require('../middlewares/validar-jwt')
+const { Router } = require("express");
+const { check } = require("express-validator");
+const {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken,
+} = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const { videos } = require("../controllers/videos");
 
-
-
-const router = Router()
+const router = Router();
 
 router.post(
-    '/new',
-    [//middlewares
-        check('name', 'El nombre es obligatorio').not().isEmpty(),
-        check('email','el email es obligatorio').isEmail(),
-        check('password', 'El password debe de ser de 6 caracteres').isLength({min:6}),
-        validarCampos
-    ]
-    ,crearUsuario)
-
- router.post(
-    '/',
-    [//middlewares
-        check('email','el email es obligatorio').isEmail(),
-        check('password', 'El password debe de ser de 6 caracteres').isLength({min:6}),
-        validarCampos
+    "/new",
+    [
+        //middlewares
+        check("name", "El nombre es obligatorio").not().isEmpty(),
+        check("email", "el email es obligatorio").isEmail(),
+        check("password", "El password debe de ser de 6 caracteres").isLength({
+            min: 6,
+        }),
+        validarCampos,
     ],
-    loginUsuario ) 
+    crearUsuario
+);
 
- router.get('/renew', validarJWT , revalidarToken)
+router.post(
+    "/",
+    [
+        //middlewares
+        check("email", "el email es obligatorio").isEmail(),
+        check("password", "El password debe de ser de 6 caracteres").isLength({
+            min: 6,
+        }),
+        validarCampos,
+    ],
+    loginUsuario
+);
 
+router.get("/renew", validarJWT, revalidarToken);
 
- module.exports = router 
+router.get("/videos", (req, res) => {
+    res.json(videos);
+});
+
+module.exports = router;
