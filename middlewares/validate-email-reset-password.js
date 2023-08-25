12b2538c0da3a1ec4d517ea59cad30fa9bc7ed14
Daniielmayo <79063734+Daniielmayo@users.noreply.github.com>
2023-08-25 15:48:28 -0500

    delete word 'contraseña'
    Co-authored-by: Daniel Calle <danielcalleco@gmail.com>

--- middlewares/validate-email-reset-password.js
+++ middlewares/validate-email-reset-password.js
@@ -16,7 +16,7 @@ const sendPasswordResetEmail = async (email, resetToken) => {
     from: user.email, //user email
     to: email,
     subject: "Restablecimiento de Contraseña",
-    html: `¡Hola! esperamos que te encuentres bien. <br/>Hemos recibido una solicitud para recuperar tu contraseña. Entendemos lo importante que es para ti acceder a tu cuenta de manera segura.  <br/>Haz clic en el siguiente enlace para restablecer tu contraseña: <br/>
+    html: `¡Hola! esperamos que te encuentres bien. <br/>Hemos recibido una solicitud para recuperar tu contraseña. Entendemos lo importante que es para ti acceder a tu cuenta de manera segura.  <br/>Haz clic en el siguiente enlace para restablecerla: <br/>
         <a href="${resetLink}">${resetLink}</a>`,
   });
 };
