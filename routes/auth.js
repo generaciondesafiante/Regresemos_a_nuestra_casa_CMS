a49cbcd86b7ccf3b8844a41561af98d4e28f23a1
Cristian Arce <cristiancamilo_1989@hotmail.com>
2023-09-14 10:50:49 -0500

    task 159 make the image editable profile

--- routes/auth.js
+++ routes/auth.js
@@ -33,6 +33,7 @@ router.post(
     check("country", "El país es obligatorio.").not().isEmpty(),
     check("city", "La cuidad es obligatoria.").not().isEmpty(),
     check("phone", "El teléfono es opcional.").not().isEmpty(),
+    check("image", "La imagen es opcional").not().isEmpty(),
 
     validateFields,
   ],
