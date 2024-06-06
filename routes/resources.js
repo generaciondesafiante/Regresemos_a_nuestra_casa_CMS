7bd1a7c17958c9e717b8c7563ce762f0a0116c1f
Daniielmayo <79063734+Daniielmayo@users.noreply.github.com>
2024-06-06 15:36:51 -0500

    Des 372 be get request resources data (#67)
    * respuesta de todos los productos
    
    * implementation of swagger tool to generate documentation in the projectresponse of all products

--- routes/resources.js
+++ routes/resources.js
@@ -5,8 +5,55 @@ const {
   validateVisibilityType,
 } = require("../middlewares/validate-resources");
 const { validateUserAndRole } = require("../middlewares/validate-rolUser");
+const allResources = require("../controllers/resources/allResources");
 
 const router = Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Resources
+ *   description: Endpoints for resource management
+ */
+
+/**
+ * @swagger
+ * /api/resources/createResource/{id}:
+ *   post:
+ *     summary: Create a new resources
+ *     description: Create a new resources
+ *     tags: [Resources]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del usuario
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               resourceUrl:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               typeResource:
+ *                 type: string
+ *               visibility:
+ *                 type: string
+ *               miniaturaUrl:
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: Recurso creado exitosamente
+ *       '400':
+ *         description: Error de solicitud inválida
+ */
 
 router.post(
   "/createResource/:id",
@@ -15,5 +62,27 @@ router.post(
   validateVisibilityType,
   createResource
 );
+
+/**
+ * @swagger
+ * /api/resources/{id}:
+ *   get:
+ *     summary: get all resources
+ *     description: get all resources
+ *     tags: [Resources]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del usuario
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Recursos obtenidos exitosamente
+ *       '404':
+ *         description: Recursos no encontrados
+ */
+router.get("/:id", validateUserAndRole, allResources);
 
 module.exports = router;
