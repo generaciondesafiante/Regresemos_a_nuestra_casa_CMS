a2ffd255764ca42d638b28a8d4067c90627d5442
daniel mayo <daniielmayo.mayo@gmail.com>
2024-06-24 12:58:11 -0500

    Implement resource data population in Courses controller to display actual resource details instead of IDs.

--- controllers/courses/addResourcesToTopic.js
+++ controllers/courses/addResourcesToTopic.js
@@ -3,7 +3,7 @@ const Resource = require("../../models/Resources");
 
 const addResourceToTopic = async (req, res) => {
   try {
-    const { courseId, topicId, resourceId } = req.body;
+    const { courseId, topicId, resourceId, isCompleted } = req.body;
 
     const course = await Course.findById(courseId);
     if (!course) {
@@ -20,13 +20,25 @@ const addResourceToTopic = async (req, res) => {
       return res.status(404).send({ error: "Tema no encontrado" });
     }
 
-    if (topic.resources.includes(resourceId)) {
+    if (
+      topic.resources.some(
+        (existingResource) => existingResource._id.toString() === resourceId
+      )
+    ) {
       return res
         .status(400)
         .send({ error: "El recurso ya está agregado a este tema" });
     }
 
-    topic.resources.push(resource._id);
+    if (course.typeOfRoute === "strict" && typeof isCompleted !== "boolean") {
+      return res.status(400).send({
+        error:
+          "Para cursos flexibles, el valor de isCompleted debe ser obligatorio",
+      });
+    }
+
+    topic.resources.push({ _id: resource._id, isCompleted });
+
     await course.save();
 
     res.status(201).send(course);
--- controllers/courses/courses.js
+++ controllers/courses/courses.js
@@ -3,7 +3,12 @@ const Course = require("../../models/Courses");
 
 const Courses = async (req, res = response) => {
   try {
-    const courses = await Course.find().populate("topic.resources");
+    const courses = await Course.find().populate({
+      path: "topic.resources",
+      populate: {
+        path: "_id", 
+      },
+    });
     res.json(courses);
   } catch (error) {
     res.status(500).json({ message: error.message });
