6185dd210b2cff328c925db22974fc620bce8be8
Daniielmayo <79063734+Daniielmayo@users.noreply.github.com>
2024-06-27 08:08:34 -0500

    Des 386 be modify attribute or flag in the lesson of the course strict (#77)
    * Implement resource data population in Courses controller to display actual resource details instead of IDs.
    
    * change of name

--- models/Courses.js
+++ models/Courses.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const courseSchema = new Schema({
-  nameCourse: { type: String, required: true },
-  titleCourse: { type: String, required: true },
-  typeOfRoute: { type: String, required: true },
-  topic: [
-    {
-      nameTopic: { type: String },
-      resources: [{ type: Schema.Types.ObjectId, ref: "Resource" }],
-    },
-  ],
-});
+const courseSchema = new Schema(
+  {
+    nameCourse: { type: String, required: true },
+    titleCourse: { type: String, required: true },
+    typeOfRoute: { type: String, required: true },
+    topic: [
+      {
+        nameTopic: { type: String },
+        resources: [
+          {
+            _id: { type: Schema.Types.ObjectId, ref: "Resource" },
+            isMandatory: { type: Boolean, required: false },
+          },
+        ],
+      },
+    ],
+  },
+  { select: { topic: { resources: { isMandatory: { $ne: null } } } } }
+);
 
 const Course = mongoose.model("Course", courseSchema);
 
