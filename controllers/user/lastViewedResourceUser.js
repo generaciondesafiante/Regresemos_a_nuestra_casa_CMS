87612606749b5860282aadc5785455f9dd99078d
daniel mayo <daniielmayo.mayo@gmail.com>
2024-08-08 09:25:58 -0500

    Implementation of the last resort seen by the user

--- controllers/user/lastViewedResourceUser.js
+++ controllers/user/lastViewedResourceUser.js
@@ -8,25 +8,30 @@ const lastViewedResource = async (req, res = response) => {
   const { userId, courseId, topicId, resourceId } = req.body;
 
   try {
+    // Validar si el userId es válido
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       return res.status(400).json({ message: "Invalid user ID format" });
     }
 
+    // Buscar el usuario por ID
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
+    // Buscar el curso por ID
     const course = await Course.findById(courseId);
     if (!course) {
       return res.status(404).json({ message: "Course not found" });
     }
 
+    // Buscar el recurso por ID
     const resource = await Resource.findById(resourceId);
     if (!resource) {
       return res.status(404).json({ message: "Resource not found" });
     }
 
+    // Buscar el tema dentro del curso
     const topic = course.topic.find(
       (topic) => topic._id.toString() === topicId
     );
@@ -34,6 +39,7 @@ const lastViewedResource = async (req, res = response) => {
       return res.status(404).json({ message: "Topic not found" });
     }
 
+    // Crear el objeto del último recurso visto
     const lastViewedResource = {
       courseName: course.nameCourse,
       courseId: course._id,
@@ -42,17 +48,16 @@ const lastViewedResource = async (req, res = response) => {
       resource: resource,
     };
 
-    user.lastViewedResources = user.lastViewedResources.filter(
-      (lr) =>
-        !(
-          lr.courseId.toString() === courseId &&
-          lr.topicId.toString() === topicId
-        )
-    );
+    // Remover cualquier registro anterior en lastViewedResources
+    user.lastViewedResources = [];
 
+    // Agregar solo el nuevo recurso visto
     user.lastViewedResources.push(lastViewedResource);
 
+    // Guardar los cambios en la base de datos
     await user.save();
+
+    // Enviar la respuesta al cliente
     return res.status(200).json({
       message: "Last viewed resource saved successfully",
       lastViewedResource,
